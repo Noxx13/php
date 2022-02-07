1a01c16d4db57c622959a6e55785fe1dfce44a00
Noxx13 <lucasochogavia.10@gail.com>
2022-02-07 16:51:58 -0300

    resolvi o conflito de olás

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 
     echo "ola mundo 123";
-<<<<<<< HEAD
 
-    echo "ola udemy"
-=======
-    
-    echo "ola hcode";
->>>>>>> f442b3858c609f8bf5c8264824571b62eac492df
+    echo "ola udemy e hcode"
 
->
+
+?>
